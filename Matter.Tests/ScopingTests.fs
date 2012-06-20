d1ea682936a29e644794b740add2c4db785dbd48
Armin Sander <armin@replicator.org>
2012-06-20 13:31:53 +0200

    add scoping tests

---
+++ Matter.Tests/ScopingTests.fs
@@ -0,0 +1,25 @@
+﻿module ScopingTests
+
+open NUnit.Framework
+
+open Syntax
+open Interpreter
+open Expression
+
+[<TestFixture>]
+type ScopingTests() =
+
+    let test l r = 
+        Assert.That(print l, Is.EqualTo(print r))
+
+    let interpretString = interpretString indentSyntax
+
+    [<Test>]
+    member this.testVisibleBelow() =
+        let str = "
+            def a 10
+            def b (if true a 11)
+            b
+            "
+        let r = interpretString str
+        test r (Number 10)
