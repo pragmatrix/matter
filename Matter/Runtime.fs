db275b455236f183fae0ab48e8920a24bb1ca210
Armin Sander <armin@replicator.org>
2012-06-29 11:25:29 +0200

    essentialFunctions => runtimeFunctions

--- Matter/Runtime.fs
+++ Matter/Runtime.fs
@@ -2,7 +2,7 @@
 
 open Expression
 
-let essentialFunctions = [
+let runtimeFunctions = [
     "list", List
     "head", fun (e::_) -> e
     "tail", fun (_::t) -> List t
@@ -12,5 +12,5 @@ let functionMap =
     let conv (name, f) =
         name, Lambda f
 
-    let pairs = List.map conv essentialFunctions
+    let pairs = List.map conv runtimeFunctions
     Map.ofList pairs
